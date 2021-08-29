14c81c4d4eb1bbaf467341842315484d9deb3ac8
Kelven Rubalaine <kelvenizzy@gmail.com>
2021-08-29 10:01:25 +0200

    comentarios para o tutorial

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const toggle = document.getElementById("toggle");
 const refresh = document.getElementById("refresh");
 const theme = window.localStorage.getItem("theme");
 
+// verifica se o tema armazenado no localStorage é escuro, se sim aplica o tema escuro ao body
 if (theme === "dark") document.body.classList.add("dark");
 
+// event listener para quando o botão de alterar o tema for clicado
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  if (theme === "dark") window.localStorage.setItem("theme", "light");
-  else window.localStorage.setItem("theme", "dark");
+  if (theme === "dark") {
+    window.localStorage.setItem("theme", "light");
+  } else window.localStorage.setItem("theme", "dark");
 });
 
 refresh.addEventListener("click", () => {
