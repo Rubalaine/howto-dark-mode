6fb457a0f04dfd5ca449bffc9a94c7c477daa62d
Kelven Rubalaine <kelvenizzy@gmail.com>
2021-08-29 10:15:19 +0200

    novo readme e novos comentarios

---
+++ Readme.md
@@ -0,0 +1,4 @@
+#Dark Mode apenas com HTML CSS e Javascript
+Repósitorio contendo o código fonte do tutorial publicado no `dev.to`.
+
+Demo: [dark-mode.kelven.dev](https://dark-mode.kelven.dev)
--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const toggle = document.getElementById("toggle");
 const refresh = document.getElementById("refresh");
 const theme = window.localStorage.getItem("theme");
 
-// verifica se o tema armazenado no localStorage é escuro, se sim aplica o tema escuro ao body
+/* verifica se o tema armazenado no localStorage é escuro
+se sim aplica o tema escuro ao body */
 if (theme === "dark") document.body.classList.add("dark");
 
 // event listener para quando o botão de alterar o tema for clicado
